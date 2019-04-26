df706a8c97ec9a91813f6b54b99665681a8358a4
Keisha Mukasa <keishamukasa@Keishas-MacBook-Pro.local>
2019-04-25 21:12:19 -0400

    Got some JS working

--- Dictionary WebApp/main.js
+++ Dictionary WebApp/main.js
@@ -1,8 +1,13 @@
+// JS for landing page of dictionary
+function init() {
+    var input = document.getElementById("input_field");
+if(input != undefined){
+    input.addEventListener('keyup', function(){
+        if(event.keyCode == 13){
+            alert(input.value);
+        }
+    }, false);
+}
+}
 
 
-var input = document.getElementById("input_field");
-input.addEventListener("keyup", function(event){
-    if(event.keyCode === 13){
-        alert(input.value);
-    }
-})
